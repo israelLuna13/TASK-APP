3501efd51724d4c455b05e33180c94196cfc5d83
israelLuna13 <c24.e.israel.luna@gmail.com>
2024-07-19 19:42:34 -0600

    Se agrego la configuracion de ts, la configuracion para conectrase a una db de mongo atlas

---
+++ task-backend/src/config/db.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+import colors from 'colors'
+import {exit} from 'node:process'
+
+export const connectDB = async() =>{
+    try {
+        const {connection} = await mongoose.connect(process.env.DATABASE_URL)
+        const url = `${connection.host}:${connection.port}}`
+        console.log(colors.magenta.bold(`MongoDB connect on: ${url}`));
+        
+    } catch (error) {
+       // console.log(error);
+       console.log(colors.red.bold('Wrong to connect MongoDB'));
+       
+        exit(1)
+    }
+}
